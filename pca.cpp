ed092f6f93fb346ec7a9eb87304987ca56fcd625
carl0545 <carl0545@vandals.uidaho.edu>
2018-04-01 11:11:16 -0700

    Created a center matrix

--- pca.cpp
+++ pca.cpp
@@ -10,15 +10,45 @@
 
 using namespace std;
 
+Matrix* center(Matrix, Matrix*); //Centers the Matrix
+
 
 int main(int argc, char *argv[]){
-  Matrix input_ppm;
+  //////DECLARATIONS//////
+  Matrix input_ppm; //input .ppm matrix
+  Matrix center;
 
-  input_ppm.readImagePpm(argv[1], "ppm");
+  //////INPUT////////
+  input_ppm.readImagePpm(argv[1], "ppm"); //read in .ppm file to matrix
+
+
+  //////CENTER MATRIX/////
+  center = new Matrix(input_ppm); //creates a copy of input matrix to use for the centered matrix
+
+  center(input_ppm, &center); //centers the 'center' matrix
 
 
 
 
+
+
+
+}
+
+void center(Matrix input, Matrix *center){
+  Matrix mean;
+  Matrix std;
+
+  mean = input->meanVec();
+  std = input->stddevVec();
+
+  for(int r = 0; r < center->numRows(); r++){
+    for(int c = 0; c < center->numCols(); c++){
+      double zscore;
+      zscore = ((input_ppm->get(r,c) - mean.get(0,c))/std.get(0,c));
+      center->set(r,c, zscore);
+    }
+  }
 
 
 }
